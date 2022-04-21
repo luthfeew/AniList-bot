718bdb3aeead724f7249c697327cb139b5573863
luthfeew <luthfeew@gmail.com>
2022-04-21 07:35:19 +0700

    welcome

--- debug.py
+++ debug.py
@@ -15,12 +15,14 @@ Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
 
+from ast import If, match_case
 import logging
 import os
+from secrets import choice
 
 from dotenv import load_dotenv
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import Update, ForceReply, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, PicklePersistence, CallbackQueryHandler
 
 load_dotenv()
 
@@ -34,13 +36,64 @@ logger = logging.getLogger(__name__)
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
+# def start(update: Update, context: CallbackContext) -> None:
+#     """Send a message when the command /start is issued."""
+#     user = update.effective_user
+#     update.message.reply_markdown_v2(
+#         fr'Hi {user.mention_markdown_v2()}\!',
+#         reply_markup=ForceReply(selective=True),
+#     )
+
+
 def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
+    """Send message on `/start`."""
+    # Get user that sent /start and log his name
+    user = update.message.from_user
+    logger.info("User %s started the conversation.", user.first_name)
+
+    reply_text1 = "Halo! Selamat datang di bot AniList (unofficial)."
+
+    keyboard = [
+        [
+            InlineKeyboardButton("Anime", callback_data='1'),
+            InlineKeyboardButton("Manga", callback_data='2'),
+        ],
+        [InlineKeyboardButton("Anime & Manga", callback_data='3')],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text(reply_text1)
+    if context.user_data:
+        # reply_text2 = context.user_data
+        reply_text2 = f"Kategori yang dipilih: {context.user_data['choice']}"
+        update.message.reply_text(reply_text2)
+    else:
+        reply_text2 = "Silahkan pilih kategori yang anda inginkan."
+        update.message.reply_text(reply_text2, reply_markup=reply_markup)
+    
+
+
+def button(update: Update, context: CallbackContext) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+
+    match query.data:
+        case '1':
+            x = 'Anime'
+        case '2':
+            x = 'Manga'
+        case '3':
+            x = 'Anime & Manga'
+
+    context.user_data['choice'] = x.lower()
+
+    query.edit_message_reply_markup(reply_markup=None)
+    query.edit_message_text(text=f"Kategori yang dipilih: {x}")
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
@@ -56,17 +109,20 @@ def echo(update: Update, context: CallbackContext) -> None:
 def main() -> None:
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
-    updater = Updater(os.getenv('TOKEN'))
+    persistence = PicklePersistence(filename='anilist_bot_data.pickle')
+    updater = Updater(os.getenv('TOKEN'), persistence=persistence)
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CallbackQueryHandler(button))
     dispatcher.add_handler(CommandHandler("help", help_command))
 
     # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(
+        Filters.text & ~Filters.command, echo))
 
     # Start the Bot
     updater.start_polling()
